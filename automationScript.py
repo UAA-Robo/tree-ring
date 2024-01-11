58516555aa244232e230304c790fbe352f45d08b
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-01-11 12:32:04 -0900

    Automation framework

--- automationScript.py
+++ automationScript.py
@@ -1,4 +1,15 @@
 class Automation:
+
     def start_automation(self):
         print("Started Automation")
+        self.get_picture()
+        self.shift_sample()
+        return
+    
+    def get_picture(self):
+        # Gets and Stores picture
+        return
+
+    def shift_sample(self):
+        # Calls arduino to turn motor
         return
